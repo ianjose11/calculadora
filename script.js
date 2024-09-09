2e1a47ef0453b37854e9a4e0bb6ee28d8cf28d02
ianjose11 <ianjose926@gmail.com>
2024-09-08 22:28:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ function back() {
     document.querySelector('.tela').innerHTML = tela.substring(0, tela.length - 1)
 }
 
-function somar(){
-    let tela =  document.querySelector('.tela').innerHTML;
+function somar() {
+    let tela = document.querySelector(".tela").innerHTML;
     if(tela) {
-         document.querySelector('.tela').innerHTML= eval(tela);
+        document.querySelector('.tela').innerHTML = eval(tela);
     } else {
         document.querySelector('.tela').innerHTML = "nenhum comando...";
     }
 }
+  
